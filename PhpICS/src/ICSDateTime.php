e8925d235b11dd5e9d700a1072a38d466661e406
Olivarès Georges <olivares.georges@gmail.com>
2013-02-14 01:48:08 +0100

    edit copy

--- PhpICS/src/ICSDateTime.php
+++ PhpICS/src/ICSDateTime.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * ICSDateTime
+ * 
+ * @author Olivarès Georges <dev@olivares-georges.fr>
+ *
+ */
 Class ICSDateTime extends \DateTime {
   public function __toString() {
     return $this->format('Ymd\THis\Z');
