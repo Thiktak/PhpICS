58a8a9f3ef44c9d6fac3013842bb355d5f15ed58
Olivarès Georges <olivares.georges@gmail.com>
2013-02-14 01:47:56 +0100

    MAJ example (index.php)

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ date_default_timezone_set('Europe/paris');
 
 include 'PhpICS/src/index.php';
 
-$icalc = ICSDocument::load('test.ics');
+$icalc = ICS::open('test.ics');
 
 foreach( $icalc as $event ) {
   echo $event->getDateStart('Y-m-d H:i:s'), ' - ', $event->getSummary();
   echo '<br />';
 }
 
-echo '<pre>', $icalc->save(), '</pre>';
+echo '<pre>', $icalc, '</pre>';
 
 
 ?>
