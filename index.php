69c798bd12f1ddea68e4e3cbc42993569b5c5033
Olivarès Georges <olivares.georges@gmail.com>
2013-02-14 02:41:53 +0100

    edit index

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ date_default_timezone_set('Europe/paris');
 
 include 'PhpICS/src/index.php';
 
+echo '<pre>';
 $icalc = ICS::open('test.ics');
+echo '</pre>';
 
 foreach( $icalc as $event ) {
   echo $event->getDateStart('Y-m-d H:i:s'), ' - ', $event->getSummary();
