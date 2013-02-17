fa475d3eccc43e00e3b3463586f566ed4c40dfde
Olivarès Georges <olivares.georges@gmail.com>
2013-02-17 22:33:32 +0100

    try{}

--- index.php
+++ index.php
@@ -6,44 +6,48 @@ date_default_timezone_set('Europe/paris');
 
 include 'PhpICS/src/index.php';
 
-$icalc = ICS::open('test.ics');
+try {
+  $icalc = ICS::open('test.ics');
 
-foreach( $icalc as $event ) {
-  echo $event->getDateStart('Y-m-d H:i:s'), ' - ', $event->getSummary();
-  echo '<br />';
+  foreach( $icalc as $event ) {
+    echo $event->getDateStart('Y-m-d H:i:s'), ' - ', $event->getSummary();
+    echo '<br />';
+  }
+
+  echo '<pre>', $icalc, '</pre>';
+
+
+  // Edit summary of the first Event
+
+  $event = $icalc->getCalendar()->getChild(0);
+  $event->setSummary('test');
+
+  echo '<pre>', $icalc, '</pre>';
+
+
+  // Create Event with addChildren(ICSVEvent $object)
+
+  $event = $icalc->getCalendar()->addChildren(new ICSVEvent());
+  $event->setDateStart(new \DateTime(null));
+  $event->setSummary('Event 1');
+
+
+  // Create Event with addChildren('event')
+
+  $event = $icalc->getCalendar()->addChildren('event');
+  $event->setDateStart(new \DateTime(null));
+  $event->setSummary('Event 2');
+
+
+  echo '<pre>', $icalc, '</pre>';
+
+  // echo $icalc
+  // echo $icalc->save();
+
+  // $icalc->save(true) save the ICalendar into test.ics
+  // $icalc->save('file.ics') save the ICalendar into `file.ics`
 }
-
-echo '<pre>', $icalc, '</pre>';
-
-
-// Edit summary of the first Event
-
-$event = $icalc->getCalendar()->getChild(0);
-$event->setSummary('test');
-
-echo '<pre>', $icalc, '</pre>';
-
-
-// Create Event with addChildren(ICSVEvent $object)
-
-$event = $icalc->getCalendar()->addChildren(new ICSVEvent());
-$event->setDateStart(new \DateTime(null));
-$event->setSummary('Event 1');
-
-
-// Create Event with addChildren('event')
-
-$event = $icalc->getCalendar()->addChildren('event');
-$event->setDateStart(new \DateTime(null));
-$event->setSummary('Event 2');
-
-
-echo '<pre>', $icalc, '</pre>';
-
-// echo $icalc
-// echo $icalc->save();
-
-// $icalc->save(true) save the ICalendar into test.ics
-// $icalc->save('file.ics') save the ICalendar into `file.ics`
-
+catch(ICSException $e) {
+  echo 'Error : ', $e->getMessage();
+}
 ?>
