410d80997ac87d657c2e2add51dcf8b1263a621e
Olivarès Georges <olivares.georges@gmail.com>
2013-02-17 22:16:47 +0100

    add documentation

--- PhpICS/src/ICS.php
+++ PhpICS/src/ICS.php
@@ -7,35 +7,75 @@
  *
  */
 Class ICS extends ICSObjects {
+  /**
+   * string $filename
+   *  Name of the file
+   */
   protected $filename;
 
+  /**
+   * string $document
+   *  Content document
+   */
   protected $document;
 
+  /**
+   * ICSObjects[] $children
+   *  List of children (ICSObjects)
+   */
   protected $children = array();
+
+  /**
+   * string[] $parsers
+   *  List of ICSObjects available with this Object
+   */
   protected $parsers = array('ICSVCalendar');
+
+
 
   public function __construct($content, $filename = null) {
     $this->filename = $filename;
     $this->content = $content;
   }
 
+  /**
+   * load
+   * @param string $content Content of the document
+   * @param string $filename
+   * @return ICS
+   */
   public static function load($content, $filename = null) {
     $doc = new self($content, $filename);
     $doc -> parse();
     return $doc;
   }
 
+  /**
+   * open
+   * @param string $filename
+   * @return ICS
+   */
   public static function open($filename) {
+    // @TODO if file exists
     $content = file_get_contents($filename);
     return self::load($content, $filename);
   }
 
+  /**
+   * getIterator
+   *   implemented from the interface IteratorAggregate
+   * @return ICS
+   */
   public function getIterator() {
     if( $this->children )
       return current($this->children)->getIterator();
     return new ArrayIterator();
   }
   
+  /**
+   * getCalendar
+   * @return ICSVCalendar
+   */
   public function getCalendar() {
     if( $this->children )
       return current($this->children);
@@ -45,6 +85,11 @@ Class ICS extends ICSObjects {
   public static function parseObject(ICSObjects $doc, $content) {
   }
 
+
+  /**
+   * saveObject
+   * @return string
+   */
   public function saveObject() {
     $return = '';
 
