9bdacec51d1de30b5ebeb078fc0d14d8bdae4336
OLIVARÈS Georges <contact@olivares-georges.fr>
2013-12-20 02:32:29 +0100

    add indent switch into test directory

--- tests/index.php
+++ tests/index.php
@@ -4,6 +4,7 @@ date_default_timezone_set('Europe/paris');
 include '../PhpICS/ICS/index.php';
 
 $get_file = filter_input(INPUT_GET, 'file', FILTER_SANITIZE_SPECIAL_CHARS) ?: '&lt;filename&gt;';
+$get_indent = (boolean) filter_input(INPUT_GET, 'indent', FILTER_SANITIZE_SPECIAL_CHARS);
 
 $FILE = 'ics/' . $get_file . '.ics';
 $icalc = null;
@@ -26,6 +27,7 @@ try {
       <?php foreach( scandir('./ics/') as $file ): if( in_array($file, array('.', '..')) ) continue; ?>
         <li><a href="?file=<?php echo pathinfo(urlencode($file), PATHINFO_FILENAME); ?>"><?php echo $file; ?></a></li>
       <?php endforeach; ?>
+        <li><a href="?file=<?php echo urlencode($get_file); ?>&amp;indent=<?php echo $get_indent == 1 ? 0 : 1; ?>">[@Indent=<?php echo $get_indent ? 'ON': 'OFF'; ?>]</a></li>
       </ul>
 
 <?php
@@ -38,7 +40,7 @@ catch(ICS\ICSException $e) { echo '<div class="alert alert-error">Error : ', $e-
 
       <h1>Output :: <em><?php echo $FILE; ?></em></h1>
       
-      <pre><h2>Generated export for <em><?php echo $FILE; ?></em></h2><code><?php echo $icalc; ?></code></pre>
+      <pre><h2>Generated export for <em><?php echo $FILE; ?></em></h2><code><?php echo $icalc($get_indent); ?></code></pre>
 
       <pre><h2><em><?php echo $FILE; ?></em></h2><code><?php echo file_exists($FILE) ? file_get_contents($FILE) : '404 Not Found'; ?></code></pre>
     </div>
